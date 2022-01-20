4d92b1dabac57ac5e1b2a012dcf48b22f162e0a8
Joshua L. Phillips <Joshua.Phillips@mtsu.edu>
2022-01-20 13:10:07 -0600

    More appropriate home directory preparation

--- config-home.sh
+++ config-home.sh
@@ -1,20 +1,36 @@
 #!/bin/bash
 
-echo "Running hook to prepare home directory: /home/${NB_USER}"
 export HOME=/home/${NB_USER}
+echo "Running hook to prepare home directory: ${HOME}"
 
 if [ ! -f ${HOME}/.profile ]; then
-   echo "Added .profile"
-   su -c "cp /etc/skel/.profile ${HOME}/." ${NB_USER}
+   echo "Adding .profile"
+   if [ "$(id -u)" == 0 ]; then
+      su -c "cp /etc/skel/.profile ${HOME}/." ${NB_USER}
+   else
+      cp /etc/skel/.profile ${HOME}/.
+   fi
 fi
 
 if [ ! -f ${HOME}/.bash_logout ]; then
-   echo "Added .bash_logout"
-   su -c "cp /etc/skel/.bash_logout ${HOME}/." ${NB_USER}
+   echo "Adding .bash_logout"
+   if [ "$(id -u)" == 0 ]; then
+      su -c "cp /etc/skel/.bash_logout ${HOME}/." ${NB_USER}
+   else
+      cp /etc/skel/.bash_logout ${HOME}/.
+   fi
 fi
 
 if [ ! -f ${HOME}/.bashrc ]; then
-   echo "Added .bashrc"
-   su -c "cp /etc/skel/.bashrc ${HOME}/." ${NB_USER}
-   su -c "conda init" ${NB_USER}
+   echo "Adding .bashrc"
+   if [ "$(id -u)" == 0 ]; then
+      su -c "cp /etc/skel/.bashrc ${HOME}/." ${NB_USER}
+      su -c "conda init" ${NB_USER}
+   else
+      cp /etc/skel/.bashrc ${HOME}/.
+      conda init
+   fi
 fi
+
+echo "Finished hook to prepare home directory: ${HOME}"
+
